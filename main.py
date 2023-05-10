e75f4f2ba54433145aba2b0bdd27a2a4cb2e2728
rudra22 <rudra22@gmail.com>
2023-05-10 01:51:45 +0100

    Update 3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from oracle_operator import OracleOperator
+from oracle_operator import OracleOperator, CursorProxy
 import oracledb
+import petl as etl
+
+
 # import cx_Oracle
 
 db_operator = OracleOperator("config.yaml")
@@ -7,28 +10,34 @@ db_operator = OracleOperator("config.yaml")
 connection = db_operator.get_conn()
 cursor = db_operator.get_cursor()
 
-for row in cursor.execute("select sysdate from dual"):
-    print(row)
 
-# ref_cursor_var = cursor.var(cx_Oracle.DB_TYPE_REFCURSOR)
-# cursor.callproc("dbms_output.enable")
-# for row in cursor.callproc("TEST_PKG.TEST_PRC", [12394, ref_cursor_var]):
+# for row in cursor.execute("select sysdate from dual"):
 #     print(row)
+#
+# cursor.execute("""
+#     declare
+#         dummy SYS_REFCURSOR;
+#         var1    NUMBER;
+#     begin
+#         dbms_output.put_line('{}');
+#     end;
+# """.format(1234))
+#
+# text_var = cursor.var(str)
+# status_var = cursor.var(int)
+# while True:
+#     cursor.callproc("dbms_output.get_line", (text_var, status_var))
+#     if status_var.getvalue() != 0:
+#         print(status_var.getvalue())
+#         break
+#     print(text_var.getvalue())
 
-cursor.execute("""
-    declare
-        dummy SYS_REFCURSOR;
-        var1    NUMBER;
-    begin
-        dbms_output.put_line('{}');
-    end;
-""".format(1234))
+def get_cursor():
+    return CursorProxy(connection.cursor())
 
-text_var = cursor.var(str)
-status_var = cursor.var(int)
-while True:
-    cursor.callproc("dbms_output.get_line", (text_var, status_var))
-    if status_var.getvalue() != 0:
-        print(status_var.getvalue())
-        break
-    print(text_var.getvalue())
+
+table = etl.fromdb(connection, 'select * from rghosh.persons')
+
+print(table)
+
+etl.todb(table, get_cursor(), 'rghosh.test1', dialect='oracle', create=False, commit=True)
